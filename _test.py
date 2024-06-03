6313dc2376ea77b2ab8734b0f3ba694d67b6f371
Járlei Rodrigues <jarlei_rodrigues@outlook.com>
2024-06-03 20:17:48 -0300

    test: unit test for update task

--- _test.py
+++ _test.py
@@ -35,3 +35,29 @@ def test_get_task():
     assert "completed" in response_json
     assert "description" in response_json
     assert "title" in response_json
+
+def test_update_task() :
+  if tasks:
+    task_id = tasks[0]
+    payload = {
+      "completed": True,
+      "description": "Nova descrição",
+      "title": "Título atualizado"
+    }
+    response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert 'message' in response_json
+
+    # Nova requisição a tarefa específica atualizada
+    response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "id" in response_json
+    assert task_id == response_json['id']
+    assert "completed" in response_json
+    assert payload["completed"] == response_json["completed"]
+    assert "description" in response_json
+    assert payload["description"] == response_json["description"]
+    assert "title" in response_json
+    assert payload["title"] == response_json["title"]
